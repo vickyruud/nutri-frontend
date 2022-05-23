92af0da95ed795d0167a7c6be8d883054676b6b7
vickyruud <aprajit.k@gmail.com>
2022-05-23 13:36:41 -0400

    get recipe data from api server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './app.css'
 
 function App() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/recipes')
+      .then((res) => {
+        setRecipes(res.data);
+        console.log(res.data[0].name)
+    })
+  }, [])
+
+
+
   return (
-      <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
+    <h1 className="text-3xl text-red-500 font-bold underline">
+      Recipes
+      {recipes ? recipes[0].name : "Hello World"}
     </h1>
   )
 }
