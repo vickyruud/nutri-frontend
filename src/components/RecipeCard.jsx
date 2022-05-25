a76f4c1106d4183219eb25eb555dec06dcbe45f1
vickyruud <aprajit.k@gmail.com>
2022-05-24 23:32:46 -0400

    improve responsive design of card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function RecipeCard({ recipe }) {
-  return (    
-    <a href='https://www.google.com/' className="flex flex-col items-center  bg-gray-200 rounded-lg border shadow-lg shadow-gray-500 dark:shadow-gray-600 md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-cyan-900 dark:hover:bg-cyan-800">
-      <img className="object-cover h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={recipe.image_url} alt="" />
-    <div className="flex flex-col justify-between p-4 leading-normal">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">{recipe.name}</h5>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-200">{recipe.description}</p>
+  return (
+    <div className="flex flex-col items-center  bg-gray-200 rounded-lg border shadow-lg shadow-gray-500 dark:shadow-gray-600 xl:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-cyan-900 dark:hover:bg-cyan-800">
+      <img
+        className="object-cover h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src={recipe.image_url}
+        alt=""
+      />
+      <div className="flex flex-col justify-between p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
+          {recipe.name}
+        </h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-200">
+          {recipe.description}
+        </p>
+      </div>
     </div>
-    </a>
-
-  )
+  );
 }
 
-export default RecipeCard
+export default RecipeCard;
