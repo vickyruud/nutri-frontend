c8f2613a50a5b8818e1347c896633a27c092dcea
vickyruud <aprajit.k@gmail.com>
2022-05-24 23:38:56 -0400

    fix styling

--- src/components/MyRecipes.jsx
+++ src/components/MyRecipes.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import RecipeList from './RecipeList';
 
 function MyRecipes({ user, recipes }) {
 
   const myRecipes = recipes.filter(recipe => recipe.user_id === user.id)
-  console.log(user);
   
   return (
     <div>
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -6,7 +6,7 @@ function NavItem({ content, onClick }) {
 
   return (
     <li>
-      <button className='text-lg font-semibold dark:text-gray-300' onClick={onClick}>{content}</button>
+      <button className='text-lg font-bold text-gray-900 dark:text-gray-300' onClick={onClick}>{content}</button>
     </li>
   )
 }
